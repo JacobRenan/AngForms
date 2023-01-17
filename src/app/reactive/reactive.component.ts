3078b4ef618b5abd49a487ff8c279570b33b37a4
Renan Jacob de Carvalho <renanjc@weg.net>
2023-01-17 08:18:45 -0300

    Final da parte de Formulários

--- src/app/reactive/reactive.component.ts
+++ src/app/reactive/reactive.component.ts
@@ -10,9 +10,11 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 //criar rota em app-rounting.module
 export class ReactiveComponent {
   
+//validators
   cadastroForm: FormGroup = this.formBuilder.group({
     firstName: ['', Validators.required],
-    lastName: ['']
+    lastName: ['', [Validators.required, Validators.minLength(4)]],
+    email: ['', [Validators.required, Validators.email]]
   });
 
   constructor(private formBuilder: FormBuilder) { }
